a67b3567572d4d617f86ec4512fd603014a042a4
Alexander Dang <alexdang1993374@gmail.com>
2023-07-12 16:52:43 -0700

    fix: padding

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ import EmployeeTable from "@/components/EmployeeTable";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 md:p-0">
+    <main className="flex min-h-screen flex-col items-center p-12 md:p-24">
       <EmployeeTable />
     </main>
   );
